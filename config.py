f9db735c8cc946699d42cc2e84723ef41bc2e1b3
acoolvoice03-ops <acoolvoice03@gmail.com>
2025-12-17 10:57:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,31 +4,30 @@ from logging.handlers import RotatingFileHandler
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
 PORT = '5010'
-OWNER_ID = 6497757690
+OWNER_ID = 8525952693
 
 MSG_EFFECT = 5046509860389126442
 
-SHORT_URL = "linkshortify.com" # shortner url 
-SHORT_API = "" 
+SHORT_URL = "gplinks.com" # shortner url 
+SHORT_API = "71c39d9ada6f3560a4a4a23d746809ef34b0485e" 
 SHORT_TUT = "https://t.me/How_to_Download_7x/26"
 
 # Bot Configuration
 SESSION = "yato"
-TOKEN = "642712"
-API_ID = ""
-API_HASH = ""
+TOKEN = "8494068620:AAFm1CsWjY6j_VbOeYNyXWezla_KJkycI8g"
+API_ID = "27806628"
+API_HASH = "25d88301e886b82826a525b7cf52e090"
 WORKERS = 5
 
-DB_URI = "mongodb"
-DB_NAME = "yato"
+DB_URI = "mongodb+srv://acoolvoice03_db_user:gDvzSlQvi0REvSWP@cluster0.t91uvkm.mongodb.net/?appName=Cluster0"
+DB_NAME = "acoolvoice03"
 
 FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
-# Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+# Database Channel (Primary)# just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
-# DB_CHANNELS = {
-#     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
-#     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
+DB_CHANNELS = {
+     "-1003392520357": {"name": "Primary DB", "is_primary": True, "is_active": True},
+    "-1003456995505": {"name": "Secondary DB", "is_primary": False, "is_active": True}
 # }
 # Auto Delete Timer (seconds)
 AUTO_DEL = 300
